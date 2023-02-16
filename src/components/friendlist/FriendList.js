d8fdabe5fd365a5731d6e7c32c32fe71cc00e277
Andrii Gargat <andreysax1312@gmail.com>
2023-02-16 16:30:12 +0200

    shape bag fixed

--- src/components/friendlist/FriendList.js
+++ src/components/friendlist/FriendList.js
@@ -1,5 +1,5 @@
 import { FriendListItem } from './FriendListItem';
-import PropTypes, { shape } from 'prop-types';
+import PropTypes from 'prop-types';
 
 export const FriendList = ({ friends }) => {
   return (
